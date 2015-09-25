3b9284092d3300b3c7937dec9d16a099eaecedd1
Aleksey Kiselev <yakudgm@gmail.com>
2015-09-25 19:12:35 +0300

    Init script

--- docker/php-fpm/init.sh
+++ docker/php-fpm/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#cd /var/www && \
-#curl -sS https://getcomposer.org/installer | php && \
-#php composer.phar update --no-dev
+cd /var/www && \
+	curl -sS https://getcomposer.org/installer | php && \
+	php composer.phar update --no-dev
 php-fpm
